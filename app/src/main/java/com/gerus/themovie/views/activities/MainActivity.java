a80c0cee65dd8b43732fd052f748f8c9cf30f8d4
Gerus-Mac <gerardo.guzmand@gmail.com>
2017-03-24 16:41:49 -0600

    Implement animation MainActivity

--- app/src/main/java/com/gerus/themovie/views/activities/MainActivity.java
+++ app/src/main/java/com/gerus/themovie/views/activities/MainActivity.java
@@ -3,12 +3,17 @@ package com.gerus.themovie.views.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
 
 import com.gerus.themovie.R;
 import com.gerus.themovie.interfaces.OnGeneralInterface;
@@ -24,8 +29,14 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements OnGeneralInterface {
     private Activity mContext;
 
-    @BindView(R.id.bottom_navigation) protected BottomNavigationView mBottonNavigation;
-    @BindView(R.id.coordinatorLayout) protected CoordinatorLayout mCoordinator;
+    @BindView(R.id.bottom_navigation)
+    protected BottomNavigationView mBottonNavigation;
+    @BindView(R.id.coordinatorLayout)
+    protected CoordinatorLayout mCoordinator;
+    @BindView(R.id.appbar)
+    protected AppBarLayout mAppBarLayout;
+    @BindView(R.id.toolbar)
+    protected Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +44,14 @@ public class MainActivity extends AppCompatActivity implements OnGeneralInterfac
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        setSupportActionBar(mToolbar);
         mContext = this;
 
         mBottonNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 GeneralFragment voFragment = null;
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.menu_payments:
                         voFragment = MoviesFragment.getInstance();
                         //mBottonNavigation.setBackgroundColor(ContextCompat.getColor(mContext,R.color.red));
@@ -49,13 +61,58 @@ public class MainActivity extends AppCompatActivity implements OnGeneralInterfac
                         //mBottonNavigation.setBackgroundColor(ContextCompat.getColor(mContext,R.color.yellow));
                         break;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragments,voFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragments, voFragment).commit();
                 return true;
             }
 
         });
 
         mBottonNavigation.findViewById(R.id.menu_payments).performClick();
+
+        mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+                Log.d(" TAG verticalOffset ", "" + verticalOffset);
+                if (verticalOffset == 0) {
+                    if (mBottonNavigation.getVisibility() != View.VISIBLE) {
+                        TranslateAnimation animate = new TranslateAnimation(0, 0, 0, 0);
+                        animate.setDuration(300);
+                        animate.setFillAfter(true);
+                        mBottonNavigation.startAnimation(animate);
+                        mBottonNavigation.setVisibility(View.VISIBLE);
+                    }
+                } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+                    if (mBottonNavigation.getVisibility() != View.GONE) {
+                        TranslateAnimation animate = new TranslateAnimation(0, 0, 0, mBottonNavigation.getHeight());
+                        animate.setDuration(300);
+                        animate.setFillAfter(true);
+                        mBottonNavigation.startAnimation(animate);
+                        mBottonNavigation.setVisibility(View.GONE);
+                    }
+                }
+                /*
+                if (verticalOffset == 0) {
+
+                    Log.d()appBarLayout.getTotalScrollRange()
+                    //mBottonNavigation.clearAnimation();
+                    //mBottonNavigation.setVisibility(View.VISIBLE);
+                    TranslateAnimation animate = new TranslateAnimation(0,0,0,mBottonNavigation.getHeight());
+                    animate.setDuration(300);
+                    animate.setFillAfter(true);
+                    mBottonNavigation.startAnimation(animate);
+                    mBottonNavigation.setVisibility(View.VISIBLE);
+                } else {
+                    //mBottonNavigation.setVisibility(View.GONE);
+                    TranslateAnimation animate = new TranslateAnimation(0,0,0,mBottonNavigation.getHeight());
+                    animate.setDuration(300);
+                    animate.setFillAfter(true);
+                    mBottonNavigation.startAnimation(animate);
+                    mBottonNavigation.setVisibility(View.GONE);
+                }
+                */
+            }
+        });
 
     }
 
@@ -72,7 +129,7 @@ public class MainActivity extends AppCompatActivity implements OnGeneralInterfac
     @Override
     public void onShowError(String psError) {
         Snackbar mSnackbar = Snackbar.make(mCoordinator, psError, Snackbar.LENGTH_LONG);
-        mSnackbar.getView().setBackgroundColor(ContextCompat.getColor(this,R.color.red));
+        mSnackbar.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.red));
         mSnackbar.show();
     }
 }

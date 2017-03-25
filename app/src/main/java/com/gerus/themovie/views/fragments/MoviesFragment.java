221604a63a6f7387d9936510cefb8580bbf90cea
Gerus-Mac <gerardo.guzmand@gmail.com>
2017-03-25 03:08:16 -0600

    Refactor MovieFragment

--- app/src/main/java/com/gerus/themovie/views/fragments/MoviesFragment.java
+++ app/src/main/java/com/gerus/themovie/views/fragments/MoviesFragment.java
@@ -8,13 +8,10 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.gerus.themovie.R;
 import com.gerus.themovie.adapters.MiniatureAdapter;
-import com.gerus.themovie.custom.CircleDisplay;
 import com.gerus.themovie.interfaces.OnDetailDialogInterface;
 import com.gerus.themovie.interfaces.OnMiniatureRecyclerInterface;
 import com.gerus.themovie.interfaces.OnWebTasksInterface;
@@ -34,15 +31,19 @@ import butterknife.BindView;
  * Created by gerus-mac on 23/03/17.
  */
 
-public class MoviesFragment extends GeneralFragment<Movie> {
+public class MoviesFragment extends GeneralFragment<Movie> implements OnMiniatureRecyclerInterface<Movie>, OnWebTasksInterface.ListResult<Movie> {
 
-    @BindView(R.id.recyclerview) RecyclerView mRecyclerView;
-    @BindView(R.id.swipeRefreshLayout) SwipeRefreshLayout mSwipeRefreshLayout;
+    @BindView(R.id.recyclerview)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
-    boolean loading = true;
-    int pastVisiblesItems, visibleItemCount, totalItemCount;
 
-    protected MiniatureAdapter mAdapter;
+    private static final int TIMER_LOADER = 1000;
+    private static final int TIMER_ANIMATION = 1500;
+
+    private boolean mFooterLoading = true;
+    private MiniatureAdapter mAdapter;
     private static int PAGE = 1;
 
     @Override
@@ -60,49 +61,118 @@ public class MoviesFragment extends GeneralFragment<Movie> {
         });
     }
 
-    public MoviesFragment() {}
+    public MoviesFragment() {
+    }
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        prcSetReyclerView();
+        prcInitRecyclerView();
+        prcSetSwipeRefresh();
 
-        //Adapter ReyclerView
-        mSwipeRefreshLayout.setColorSchemeColors(ContextCompat.getColor(mContext,R.color.red));
+        if (UNetwork.isOnline(mContext)) {
+            prcWebGetGeners();
+        } else {
+            prcUpdateRecyclerView(mDB.getListMovies());
+        }
+    }
+
+    private void prcInitRecyclerView() {
+        final GridLayoutManager mLayoutManager = new GridLayoutManager(mContext, getResources().getInteger(R.integer.numberColumns));
+        mLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return mAdapter.getNumberColumns(position);
+            }
+        });
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                // Scroll bottom
+                if (dy > 0 && mFooterLoading) {
+                    int viItemsHidden, viItemsVisible, viItemTotal;
+                    viItemsVisible = mLayoutManager.getChildCount();
+                    viItemTotal = mLayoutManager.getItemCount();
+                    viItemsHidden = mLayoutManager.findFirstVisibleItemPosition();
+
+                    if ((viItemsVisible + viItemsHidden) >= viItemTotal) {
+                        mFooterLoading = false;
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                prcWebGetMovies(false);
+                            }
+                        }, TIMER_LOADER);
+                    }
+                }
+            }
+        });
+
+        mAdapter = new MiniatureAdapter(mContext, mListMiniatures, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
+        itemAnimator.setChangeDuration(TIMER_ANIMATION);
+        mRecyclerView.setItemAnimator(itemAnimator);
+    }
+
+
+    /**
+     * Method to manipulate swipeRefresh
+     */
+    private void prcSetSwipeRefresh() {
+        mSwipeRefreshLayout.setColorSchemeColors(ContextCompat.getColor(mContext, R.color.red));
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                if(UNetwork.isOnline(mContext)){
-                    prcGenre();
+                if (UNetwork.isOnline(mContext)) {
+                    prcWebGetGeners();
                 } else {
                     prcCleanError(mContext.getString(R.string.error_network));
                 }
             }
         });
+    }
 
-        if(UNetwork.isOnline(mContext)){
-            prcGenre();
-        } else {
-            updateRecyclerView(mDB.getListMovies());
+    /**
+     * Method update Recyclerview values
+     *
+     * @param poMovieList
+     */
+    private void prcUpdateRecyclerView(List<Movie> poMovieList) {
+        mDB.saveMovies(poMovieList);
+        for (int i = 0; i < poMovieList.size(); i++) {
+            mListMiniatures.add(poMovieList.get(i));
+            mAdapter.notifyItemChanged(i);
         }
-
     }
 
+    /**
+     * Show error mesage and reset values
+     *
+     * @param psError
+     */
     private void prcCleanError(String psError) {
-        loading = true;
+        mFooterLoading = true;
+        //Remove progress bar
         mAdapter.notifyItemRemoved(mAdapter.getItemCount());
-        mListener.onShowError(psError);
         mSwipeRefreshLayout.setRefreshing(false);
+        if (mListener != null) mListener.onShowError(psError);
     }
 
-    private void prcGenre() {
+    /**
+     * Method to call Geners and save DB
+     */
+    private void prcWebGetGeners() {
 
         mWebTasks.prcGetGenreMovies(new OnWebTasksInterface.GenreResult() {
             @Override
             public void onResult(List<Genre> poGenreList) {
                 mDB.saveGenesMovies(poGenreList);
-                prcRefreshValues(true);
+                prcWebGetMovies(true);
             }
 
             @Override
@@ -113,132 +183,77 @@ public class MoviesFragment extends GeneralFragment<Movie> {
     }
 
     /**
-     * Get values form server
+     * Get new movies from server
+     *
+     * @param pbReset Reset number of Pages
      */
-    private void prcRefreshValues(final boolean pbReset) {
-        PAGE  = (pbReset) ? 1 : PAGE;
-        mWebTasks.prcGetMovies(PAGE, new OnWebTasksInterface.ListResult<Movie>() {
-
-            @Override
-            public void onResult(List<Movie> poMovieList, int piPages) {
-
-                loading = true;
-                mSwipeRefreshLayout.setRefreshing(false);
-                if(PAGE > piPages){
-                    onError(mContext.getString(R.string.error_limit));
-                } else {
-                    if(pbReset || PAGE == 1){
-                        mListMiniatures.clear();
-                        mAdapter.resetType();
-                        mAdapter.notifyDataSetChanged();
-                        mDB.clearTableMovies();
-                    }
-                    PAGE = PAGE + 1;
-                    updateRecyclerView(poMovieList);
-                }
-
-
-            }
-
-            @Override
-            public void onError(String psErrorMsg) {
-                prcCleanError(psErrorMsg);
-            }
-        });
+    private void prcWebGetMovies(final boolean pbReset) {
+        PAGE = (pbReset) ? 1 : PAGE;
+        mWebTasks.prcGetMovies(PAGE, this);
     }
 
-    private void updateRecyclerView(List<Movie> poMovieList) {
-        mDB.saveMovies(poMovieList);
-        for (int i = 0; i < poMovieList.size(); i++) {
-            mListMiniatures.add(poMovieList.get(i));
-            mAdapter.notifyItemChanged(i);
+
+    @Override
+    public void onItemSelected(Movie poMovie) {
+        mListener.onItemSelected(poMovie);
+    }
+
+    @Override
+    public void onRefreshValues(int piType) {
+        switch (piType) {
+            case MiniatureAdapter.TYPE_POPULAR:
+                Collections.sort(mListMiniatures, new Comparator<Movie>() {
+
+                    @Override
+                    public int compare(Movie o1, Movie o2) {
+                        return ((Double) o2.getPopularity()).compareTo(o1.getPopularity());
+                    }
+                });
+                break;
+            case MiniatureAdapter.TYPE_RATED:
+                Collections.sort(mListMiniatures, new Comparator<Movie>() {
+
+                    @Override
+                    public int compare(Movie o1, Movie o2) {
+                        return ((Double) o2.getVote_average()).compareTo(o1.getVote_average());
+                    }
+                });
+                break;
+            case MiniatureAdapter.TYPE_UPCOMING:
+                Collections.sort(mListMiniatures, new Comparator<Movie>() {
+
+                    @Override
+                    public int compare(Movie o1, Movie o2) {
+                        return o2.getRelease_date().compareTo(o1.getRelease_date());
+                    }
+                });
+                break;
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+
+
+    @Override
+    public void onResult(List<Movie> poMovieList, int piPages) {
+        mFooterLoading = true;
+        mSwipeRefreshLayout.setRefreshing(false);
+        if (PAGE > piPages) {
+            onError(mContext.getString(R.string.error_limit));
+        } else {
+            // IF page 1, reset all values
+            if (PAGE == 1) {
+                mListMiniatures.clear();
+                mAdapter.resetType();
+                mAdapter.notifyDataSetChanged();
+                mDB.clearTableMovies();
+            }
+            PAGE = PAGE + 1;
+            prcUpdateRecyclerView(poMovieList);
         }
     }
 
-    private void prcSetReyclerView() {
-        final GridLayoutManager mLayoutManager = new GridLayoutManager(mContext, getResources().getInteger(R.integer.numberColumns));
-        mLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
-            @Override
-            public int getSpanSize(int position) {
-                return mAdapter.getNumberColumns(position);
-            }
-        });
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy)
-            {
-                if(dy > 0) { //check for scroll down
-
-                    visibleItemCount = mLayoutManager.getChildCount();
-                    totalItemCount = mLayoutManager.getItemCount();
-                    pastVisiblesItems = mLayoutManager.findFirstVisibleItemPosition();
-
-                    if (loading) {
-                        if ( (visibleItemCount + pastVisiblesItems) >= totalItemCount) {
-                            loading = false;
-
-                            new Handler().postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    prcRefreshValues(false);
-                                }
-                            },1000);
-                        }
-                    }
-                }
-            }
-        });
-
-        mAdapter = new MiniatureAdapter(mContext, mListMiniatures, new OnMiniatureRecyclerInterface<Movie>() {
-
-            @Override
-            public void onItemSelected(Movie poMovie) {
-                mListener.onItemSelected(poMovie);
-            }
-
-
-            @Override
-            public void onRefreshValues(int piType) {
-                switch (piType){
-                    case MiniatureAdapter.TYPE_POPULAR:
-                        Collections.sort(mListMiniatures, new Comparator<Movie>() {
-
-                            @Override
-                            public int compare(Movie o1, Movie o2) {
-                                return ((Double) o2.getPopularity()).compareTo(o1.getPopularity());
-                            }
-                        });
-                        break;
-                    case MiniatureAdapter.TYPE_RATED:
-                        Collections.sort(mListMiniatures, new Comparator<Movie>() {
-
-                            @Override
-                            public int compare(Movie o1, Movie o2) {
-                                return ((Double) o2.getVote_average()).compareTo(o1.getVote_average());
-                            }
-                        });
-                        break;
-                    case MiniatureAdapter.TYPE_UPCOMING:
-                        Collections.sort(mListMiniatures, new Comparator<Movie>() {
-
-                            @Override
-                            public int compare(Movie o1, Movie o2) {
-                                return o2.getRelease_date().compareTo(o1.getRelease_date());
-                            }
-                        });
-                        break;
-                }
-                mAdapter.notifyDataSetChanged();
-            }
-        });
-        mRecyclerView.setAdapter(mAdapter);
-
-        RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
-        itemAnimator.setChangeDuration(1500);
-        mRecyclerView.setItemAnimator(itemAnimator);
+    @Override
+    public void onError(String psErrorMsg) {
+        prcCleanError(psErrorMsg);
     }
-
 }
